8eb3e81bdf4f479b997a810b9242c9969b8101a9
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-02-08 16:43:10 +0530

    Added React Native app as part of the main repository

---
+++ PhoneApplication/App.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "./global.css"
+
+const App = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-3xl text-center'>App initialized with nativeWind</Text>
+    </View>
+  )
+}
+
+export default App
