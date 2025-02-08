dfc5dfcf9d855d3e20e5bf11734a4d1a0b9fa31d
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-02-08 19:28:00 +0530

    Initialized the routes

---
+++ PhoneApplication/src/routes/01_StackScreens.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+
+//Screens
+import Login from '../screens/0_Login'
+import Register from '../screens/0_Register'
+
+
+const stack = createStackNavigator()
+
+
+const StackScreens = () => {
+  return (
+    <stack.Navigator>
+        <stack.Screen name="Login" component={Login} />
+        <stack.Screen name="Register" component={Register} />
+    </stack.Navigator>
+  )
+}
+
+export default StackScreens
