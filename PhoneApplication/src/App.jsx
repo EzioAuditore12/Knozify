ce7ee3174055b760f67447defb88b7ac59c45734
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-02-08 16:45:34 +0530

    Initialized the PhoneApplication with nativeWind

--- PhoneApplication/src/App.jsx
+++ PhoneApplication/src/App.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import "./global.css"
+import "../global.css"
 
 const App = () => {
   return (
-    <View className='flex-1 justify-center items-center'>
+    <View className='flex-1 justify-center items-center bg-red-500'>
       <Text className='text-3xl text-center'>App initialized with nativeWind</Text>
     </View>
   )
