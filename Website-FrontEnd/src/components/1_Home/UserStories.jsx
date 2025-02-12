4f90d136e978d21a14de2550dd2ff31626e4544a
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-02-12 20:20:48 +0530

    Updated website mediium device responsiveness

--- Website-FrontEnd/src/components/1_Home/UserStories.jsx
+++ Website-FrontEnd/src/components/1_Home/UserStories.jsx
@@ -9,10 +9,18 @@ function UserBox() {
         { id: 5, image: 'https://i.pravatar.cc/150?img=5', status: 'Online' },
         { id: 6, image: 'https://i.pravatar.cc/150?img=6', status: 'Offline' },
         { id: 7, image: 'https://i.pravatar.cc/150?img=7', status: 'Busy' },
+        { id: 8, image: 'https://i.pravatar.cc/150?img=8', status: 'Online' },
+        { id: 9, image: 'https://i.pravatar.cc/150?img=9', status: 'Away' },
+        { id: 10, image: 'https://i.pravatar.cc/150?img=10', status: 'Online' },
+        { id: 11, image: 'https://i.pravatar.cc/150?img=11', status: 'Busy' },
+        { id: 12, image: 'https://i.pravatar.cc/150?img=12', status: 'Offline' },
+        { id: 13, image: 'https://i.pravatar.cc/150?img=13', status: 'Online' },
+        { id: 14, image: 'https://i.pravatar.cc/150?img=14', status: 'Away' },
+        { id: 15, image: 'https://i.pravatar.cc/150?img=15', status: 'Online' }
     ]
 
     return (
-        <div className='flex flex-row overflow-x-scroll snap-x snap-mandatory w-full bg-white p-2 rounded-lg shadow-md scrollbar-hide'>
+        <div className='flex flex-row overflow-x-scroll snap-x snap-mandatory w-full bg-white p-2 rounded-lg shadow-md scrollbar-hide mr-16'>
             {users.map(user => (
                 <div key={user.id} className='snap-start flex-shrink-0 w-[110px] flex flex-col items-center'>
                     <img 
--- Website-FrontEnd/src/pages/features/1_Home.jsx
+++ Website-FrontEnd/src/pages/features/1_Home.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import UserStories from '../../components/1_Home/UserStories'
 import UserPosts from '../../components/1_Home/UserPosts';
 import SearchFriends from '../../components/1_Home/searchFriends';
+import { useMediumDevice } from '../../contexts';
+import { useLargeDevice } from '../../contexts';
+import { useExtraLargeDevice } from '../../contexts/Device_Sizes/ExtraLargeDeviceContext';
+
 
 const Home = () => {
+  const { isMediumDevice } = useMediumDevice();
+  const {isLargeDevice} = useLargeDevice(); 
+  const {isExtraLargeDevice} = useExtraLargeDevice();
   return (
     <div className='mt-16 md:ml-[80px] mb-[50px] relative'>
+
+    <div className='flex flex-col md:mr-[100px]'>
       <UserStories/>
       <UserPosts/>
-      <SearchFriends/>
+    </div>  
+      {(isMediumDevice || isLargeDevice || isExtraLargeDevice) && <SearchFriends/>}
     </div>
   )
 }
