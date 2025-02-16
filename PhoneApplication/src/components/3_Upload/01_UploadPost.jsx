b9f8394e9d91798f51b4b39b766660a5cd7ec962
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2025-02-16 14:30:50 +0530

    Updated logic of post uploading

--- PhoneApplication/src/components/3_Upload/01_UploadPost.jsx
+++ PhoneApplication/src/components/3_Upload/01_UploadPost.jsx
@@ -3,36 +3,45 @@ import React, { useState } from 'react'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 import { pick, types } from '@react-native-documents/picker'
+import { useSelector } from 'react-redux'
 import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const UploadPost = ({}) => {
-  const [fileData, setFileData] = useState([])
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [selectedVideo, setSelectedVideo] = useState(null)
+ 
+  const { user } = useSelector((state) => state.auth)
 
-  const handlePick = async () => {
+  const handlePickImage = async () => {
     try {
       const response = await pick({
         presentationStyle: 'fullScreen',
         allowMultiSelection: false,
-        type: [types.images, types.video]
+        type: [types.images]
       })
-      console.log(response)
-      setFileData(response)
+      if(response && response.length > 0){
+        setSelectedImage(response[0])
+      }
     } catch (err) {
       console.log(err)
     }
   }
 
-  const handleRemoveFile = (index) => {
-    const newFiles = [...fileData]
-    newFiles.splice(index, 1)
-    setFileData(newFiles)
+  const handlePickVideo = async () => {
+    try {
+      const response = await pick({
+        presentationStyle: 'fullScreen',
+        allowMultiSelection: false,
+        type: [types.video]
+      })
+      if(response && response.length > 0){
+        setSelectedVideo(response[0])
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
-
-  const isVideo = (file) => {
-    return file.type?.toLowerCase().includes('video') ||
-      file.uri?.toLowerCase().includes('.mp4') ||
-      file.uri?.toLowerCase().includes('.mov')
-  };
 
   const validationSchema = yup.object().shape({
     // Post Title
@@ -47,16 +56,16 @@ const UploadPost = ({}) => {
 
   return (
     <ScrollView>
-      <View className='p-2 flex-1 '>
+      <View className='p-2 flex-1'>
         <Text className='text-black font-bold text-2xl mb-[20px]'>Upload Post</Text>
         <View className='flex-row gap-x-1'>
           <Image source={{
-            uri: 'https://res.cloudinary.com/dpcloud123/image/upload/v1737164154/avmrjdkmjr116rxu5uis.jpg'
+            uri: user.profile_picture
           }}
           className='w-[60px] h-[60px] rounded-full'
           />
           <View className='flex-col'>
-            <Text className='text-black font-bold'>Daksh</Text>
+            <Text className='text-black font-bold'>{user.user_name}</Text>
             <Text className='text-gray-400'>Public</Text>
           </View>
         </View>
@@ -64,7 +73,10 @@ const UploadPost = ({}) => {
           validationSchema={validationSchema}
           initialValues={{ post_title: '', post_content: '', post_video: '', post_photo: '' }}
           onSubmit={(values) => {
-            console.log(values)
+            console.log({ ...values,
+              post_photo: selectedImage ? selectedImage.uri : '',
+              post_video: selectedVideo ? selectedVideo.uri : ''
+            })
           }}
         >
           {({
@@ -77,7 +89,7 @@ const UploadPost = ({}) => {
             isValid
           }) => (
             <>
-              <View className='p-2 flex-1 flex-col gap-y-4'>
+              <View className='p-2 flex-col gap-y-4'>
                 <TextInput
                   className='border-2 rounded-lg'
                   placeholder='Post Title'
@@ -87,7 +99,42 @@ const UploadPost = ({}) => {
                   onBlur={handleBlur('post_title')}
                   value={values.post_title}
                 />
-                {errors.post_title && touched.post_title && <Text>{errors.post_title}</Text>}
+                {errors.post_title && touched.post_title && <Text className='text-red-500'>{errors.post_title}</Text>}
+
+                {/* Media Options Section: Only one of photo or video can be selected */}
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 8 }}>
+                  {(!selectedImage && !selectedVideo) ? (
+                    <>
+                      <TouchableOpacity onPress={handlePickImage}>
+                        <Icon name="image" size={30} color="#000" />
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={handlePickVideo}>
+                        <Icon name="videocam" size={30} color="#000" />
+                      </TouchableOpacity>
+                    </>
+                  ) : (
+                    <>
+                      {selectedImage && (
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                          <Image source={{ uri: selectedImage.uri }} style={{ width: 60, height: 60, borderRadius: 4 }} />
+                          <TouchableOpacity onPress={() => setSelectedImage(null)}>
+                            <Icon name="close" size={20} color="red" style={{ marginLeft: 8 }} />
+                          </TouchableOpacity>
+                        </View>
+                      )}
+                      {selectedVideo && (
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                          <View style={{ width: 60, height: 60, backgroundColor: '#ccc', justifyContent: 'center', alignItems: 'center' }}>
+                            <Icon name="videocam" size={30} color="#000" />
+                          </View>
+                          <TouchableOpacity onPress={() => setSelectedVideo(null)}>
+                            <Icon name="close" size={20} color="red" style={{ marginLeft: 8 }} />
+                          </TouchableOpacity>
+                        </View>
+                      )}
+                    </>
+                  )}
+                </View>
 
                 <TextInput
                   className='border-2 rounded-lg min-h-[230px]'
@@ -100,35 +147,9 @@ const UploadPost = ({}) => {
                   value={values.post_content}
                   multiline={true}
                 />
-                {/* File Picker Section placed at the bottom */}
-              <View className='mt-4'>
-                <Button title="Select File" onPress={handlePick} />
-                {fileData.length > 0 && (
-                  <View className='mt-4'>
-                    <Text className='font-bold mb-2'>Selected Files:</Text>
-                    {fileData.map((file, index) => (
-                      <View key={index} className='flex-row items-center mb-2'>
-                        {!isVideo(file) ? (
-                          <Image source={{ uri: file.uri }} style={{ width: 60, height: 60, borderRadius: 4 }} />
-                        ) : (
-                          <View style={{ width: 60, height: 60, backgroundColor: '#ccc', justifyContent: 'center', alignItems: 'center' }}>
-                            <Text>Video</Text>
-                          </View>
-                        )}
-                        <Text className='ml-2 flex-1' numberOfLines={1}>{file.uri}</Text>
-                        <TouchableOpacity onPress={() => handleRemoveFile(index)}>
-                          <Text style={{ color: 'red', marginLeft: 8 }}>Remove</Text>
-                        </TouchableOpacity>
-                      </View>
-                    ))}
-                  </View>
-                )}
-              </View>
-                {errors.post_content && touched.post_content && <Text>{errors.post_content}</Text>}
+                {errors.post_content && touched.post_content && <Text className='text-red-500'>{errors.post_content}</Text>}
                 <Button onPress={handleSubmit} title="Submit" disabled={!isValid} />
               </View>
-
-              
             </>
           )}
         </Formik>
